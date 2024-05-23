88f1e054f1c76c5816f82a2804abe95dc971c318
syed milad <syedmilad99@gmail.com>
2024-05-23 22:50:23 +0500

    removing the template

--- vue-project-for-learning-purpose/src/components/InputForm.vue
+++ vue-project-for-learning-purpose/src/components/InputForm.vue
@@ -1,37 +1,13 @@
-<template>
+<template >
   <div>
-  <div v-for="(item, index) in formItems" :key="index">
-    {{ item.lable }}
-    <template v-if="item.type === 'color'">
-      <input  type="color" v-modle="item.value" @input="updateItems(index, $event.target.value)"/>
-    </template>
-    <template v-else-if="item.type === 'checkbox'">
-      <input  type="checkbox" v-modle="item.value"  @change="updateItems(index, $event.target.checked)" />
-    </template>
+    <h1>Footer</h1>
   </div>
-  <button @click="handler">Submit</button>
-</div>
 </template>
-
 <script>
-  export default{
-    'name': 'input-form',
-    data(){
-      return {
-        formItems:[
-          {lable: 'Candle-Color',type: 'color', value: '#ffffff'},
-          {lable: 'Magnet',type: 'checkbox', value: false},
-        ],
-        data: []
-      }
-    },
-    methods:{
-      updateItems(index, eventValue){
-          this.formItems[index].value = eventValue;
-      },
-      handler(){
-        console.log(this.formItems)
-      }
-    }
-  }
+export default {
+  
+}
 </script>
+<style >
+  
+</style>
